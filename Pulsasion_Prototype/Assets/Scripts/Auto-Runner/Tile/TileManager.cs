d367102625a92fc9da85a2c11ded4c40dafbdc08
Vision159 <99491625+Vision159@users.noreply.github.com>
2022-04-13 01:44:07 +0200

    Tiles

--- Pulsasion_Prototype/Assets/Scripts/Auto-Runner/Tile/TileManager.cs
+++ Pulsasion_Prototype/Assets/Scripts/Auto-Runner/Tile/TileManager.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class TileManager : MonoBehaviour
 {
+    public GameObject[] tilePrefabs;
+
+    public float zSpawn = 50;
+
+    public float tileLenght = 30;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnTile(0);
+        SpawnTile(1);
+        SpawnTile(2);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SpawnTile(int tileIndex)
+    {
+        Instantiate(tilePrefabs[tileIndex], transform.forward * zSpawn, transform.rotation);
+        zSpawn += tileLenght;
     }
 }
