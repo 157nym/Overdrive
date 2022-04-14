9b0ef5037d45a728459470fa181a5a84708b0fd8
Ønym <onym.balz@gmail.com>
2022-04-14 15:16:40 +0200

    Updata WWise

--- Overdrive/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Overdrive/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,42 +11,6 @@
 
 namespace AK
 {
-    namespace EVENTS
-    {
-        static const AkUniqueID DASH = 1942692385U;
-        static const AkUniqueID JUMP_LAND = 3419686739U;
-        static const AkUniqueID JUMP_START = 3244792144U;
-        static const AkUniqueID MUSIC = 3991942870U;
-        static const AkUniqueID STEP = 621108255U;
-    } // namespace EVENTS
-
-    namespace SWITCHES
-    {
-        namespace CURRENT_MUSIC
-        {
-            static const AkUniqueID GROUP = 4171319444U;
-
-            namespace SWITCH
-            {
-                static const AkUniqueID EUROBEAT = 3282784734U;
-                static const AkUniqueID MUSIC_1 = 508837814U;
-                static const AkUniqueID MUSIC_2 = 508837813U;
-            } // namespace SWITCH
-        } // namespace CURRENT_MUSIC
-
-    } // namespace SWITCHES
-
-    namespace GAME_PARAMETERS
-    {
-        static const AkUniqueID MUSIC_SPEED = 2537816454U;
-        static const AkUniqueID SPEED = 640949982U;
-    } // namespace GAME_PARAMETERS
-
-    namespace TRIGGERS
-    {
-        static const AkUniqueID DASH = 1942692385U;
-    } // namespace TRIGGERS
-
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
