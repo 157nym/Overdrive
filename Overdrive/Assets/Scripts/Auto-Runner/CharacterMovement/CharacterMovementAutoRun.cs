29598e8a54935b3f9e6ee2f8bdfe81c1701e9158
Ønym <onym.balz@gmail.com>
2022-04-16 11:29:06 +0200

    FIX

--- Overdrive/Assets/Scripts/Auto-Runner/CharacterMovement/CharacterMovementAutoRun.cs
+++ Overdrive/Assets/Scripts/Auto-Runner/CharacterMovement/CharacterMovementAutoRun.cs
@@ -36,8 +36,8 @@ public class CharacterMovementAutoRun : MonoBehaviour
         
         direction.z = forwardSpeed;
 
-        //if(EventSystem.current.IsPointerOverGameObject())
-            //return;
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
 
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
