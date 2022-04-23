cfb90618673e32f02cfacd0fef425187fea4edec
Luigi De Angelis <luigijose15@gmail.com>
2022-04-23 15:53:26 +0200

    popup

--- Overdrive/Assets/Scripts/Tactile/SpawnPopUp.cs
+++ Overdrive/Assets/Scripts/Tactile/SpawnPopUp.cs
@@ -12,11 +12,23 @@ public class SpawnPopUp : MonoBehaviour
     public Canvas canvas;
 
     public float SpawnRate;
+
+     public RectTransform rt;
     // Update is called once per frame
+
+    public void Start()
+    {
+
+    }
 
     public IEnumerator SpawnPop()
     {
-        RawImage Spawn = Instantiate(popUp[0], target.position,target.rotation, canvas.transform);
+        float spawnX = rt.transform.position.x + Random.Range(-rt.gameObject.transform.position.x + popUp[0].rectTransform.rect.height/2, rt.gameObject.transform.position.x);
+        float spawnY = rt.transform.position.y + Random.Range(-rt.gameObject.transform.position.y + popUp[0].rectTransform.rect.width/2, rt.gameObject.transform.position.y);
+
+        Vector2 spawnPosition = new Vector2(spawnX,spawnY);
+
+        RawImage Spawn = Instantiate(popUp[0], spawnPosition , Quaternion.identity, target.transform);
         yield return new WaitForSeconds(SpawnRate);
     }
 }
