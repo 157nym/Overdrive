550a5bb80ab2952dceb5a1fbcc6bc95f7f30f8bf
Lhollie <62471942+SylarBlood@users.noreply.github.com>
2022-05-06 18:28:21 +0200

    LD 06/05 fixed

--- Assets/Scripts/Auto-Runner/Tile/TileManager.cs
+++ Assets/Scripts/Auto-Runner/Tile/TileManager.cs
@@ -28,6 +28,8 @@ public class TileManager : MonoBehaviour
             if (i == 0)
             {
                 SpawnTile(0);
+                SpawnTile(0);
+                SpawnTile(0);
             }
 
             else
@@ -49,12 +51,6 @@ public class TileManager : MonoBehaviour
                     int ram = Random.Range(0,4);
                     SpawnTile(ram);
                 }
-                
-                if (Verif == 7)
-                {
-                    Hauteur += 1;
-                    Split(1);
-                }
             }
         }
     }
@@ -62,7 +58,7 @@ public class TileManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (playerTransform.position.z - 35 > zSpawn-(tilePrefabs.Length * tileLenght))
+        if (playerTransform.position.z - 200 > zSpawn-(tilePrefabs.Length * tileLenght))
         {
                 int Verif = Random.Range(0,tilePrefabs.Length);
 
@@ -82,11 +78,6 @@ public class TileManager : MonoBehaviour
                     SpawnTile(ram);
                 }
 
-                if (Verif == 7)
-                {
-                    Hauteur += 1;
-                    Split(1);
-                }
                 DeleteTile();
         }
     }
@@ -97,20 +88,6 @@ public class TileManager : MonoBehaviour
         pos.y = Hauteur * 10;
         GameObject go = Instantiate(tilePrefabs[tileIndex], pos, transform.rotation);
         activeTiles.Add(go);
-        zSpawn += tileLenght;
-    }
-
-    public void Split(int tileIndex)
-    {
-        Vector3 pos = transform.forward * zSpawn;
-        pos.y = Hauteur * 10;
-        GameObject go = Instantiate(tilePrefabs[tileIndex], pos, transform.rotation);
-        activeTiles.Add(go);
-
-        Vector3 pos2 = transform.forward;
-        pos.y = Hauteur;
-        GameObject go2 = Instantiate(tilePrefabs[tileIndex], pos2, transform.rotation);
-        activeTiles.Add(go2);
         zSpawn += tileLenght;
     }
 
