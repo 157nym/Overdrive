0f12253c1ba897d45d44ea8b520c181c1201c003
LeSelDiscret <leseldiscret@gmail.com>
2022-05-09 00:49:46 +0200

    peaufinage

---
+++ Overdrive/Assets/Scripts/Score.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Score : MonoBehaviour
+{
+    CharacterMovementAutoRun speeds;
+    Text vitesse;
+    public float vitessejoueur;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        vitesse = GetComponent<Text>();
+        vitessejoueur = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        vitesse.text = "Score Corpo = " + vitessejoueur.ToString("f1") + " pts";
+
+        vitessejoueur = vitessejoueur + 1f*Time.deltaTime;
+    }
+
+}
