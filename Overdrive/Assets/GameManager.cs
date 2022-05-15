3fbff3aa1a0bfce2d8d0af4b38b9c250f7a53b43
Vision159 <99491625+Vision159@users.noreply.github.com>
2022-05-15 18:18:51 +0200

    Coins v1

--- Overdrive/Assets/GameManager.cs
+++ Overdrive/Assets/GameManager.cs
@@ -4,20 +4,10 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public int coins;
     public bool Paused = false;
     public GameObject Continue, Menu, Option, PopUpManager;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void PauseGame()
     {
         Paused = !Paused;
