2c522049a88c16d1d927b2cad56b14130cf8a31d
Ønym <onym.balz@gmail.com>
2022-05-20 17:21:29 +0200

    HUD Life Anim

---
+++ Overdrive/Assets/Life_Hud.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Life_Hud : MonoBehaviour
+{
+    public int Life = 4;
+
+    public Sprite[] Life_Tex;
+
+    public Image bar;
+
+    private void Start()
+    {
+        bar.sprite = Life_Tex[Life - 1];
+    }
+
+    public void Damage()
+    {
+        Life--;
+        if(Life >= 0)  bar.sprite = Life_Tex[Life - 1];
+    }
+}
